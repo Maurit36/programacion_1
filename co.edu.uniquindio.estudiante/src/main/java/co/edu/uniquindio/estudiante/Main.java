54b59f0ee9de4f45b3d3e9e0e8043cb08410e9c4
Maurit36 <nelsonf.gallegos@uqvirtual.edu.co>
2023-08-25 17:39:01 -0500

    Avance Proyecto Estudiante

--- co.edu.uniquindio.estudiante/src/main/java/co/edu/uniquindio/estudiante/Main.java
+++ co.edu.uniquindio.estudiante/src/main/java/co/edu/uniquindio/estudiante/Main.java
@@ -1,5 +1,7 @@
 package co.edu.uniquindio.estudiante;
 
+import java.util.Scanner;
+
 //Importación clase Estudiante
 import co.edu.uniquindio.estudiante.model.Estudiante;
 
@@ -40,11 +42,17 @@ public class Main {
         estudiante3.setNota2(2.5);
         estudiante3.setNota3(4.5);
 
-        calcularDefinitivaEstudiante(estudiante1, estudiante2, estudiante3);
-        //calcularPromedioCurso(estudiante1, estudiante2, estudiante3);
+        defEstudiante1(estudiante1);
+        defEstudiante2(estudiante2);
+        defEstudiante3(estudiante3);
+        //calcularDefinitivaEstudiante(estudiante1, estudiante2, estudiante3);
+        calcularPromedioCurso(estudiante1, estudiante2, estudiante3);
         calcularPromedioEdad(estudiante1, estudiante2, estudiante3);
         calcularPromedioNota1(estudiante1, estudiante2, estudiante3);
-        calcularNotaMayorDelCurso(estudiante1, estudiante2, estudiante3);
+        calcularNotaMayorEst1(estudiante1);
+        calcularNotaMayorEst2(estudiante2);
+        calcularNotaMayorEst3(estudiante3);
+        //calcularNotaMayorDelCurso(estudiante1, estudiante2, estudiante3);
         //calcularNotaMenorCurso(estudiante1, estudiante2, estudiante3);
         //aprobacionCurso(estudiante1, estudiante2, estudiante3);
         //calcularPorcentajeGanaronCurso(estudiante1, estudiante2, estudiante3);
@@ -53,31 +61,67 @@ public class Main {
         //notaMaxima(estudiante1, estudiante2, estudiante3);
     }
 
-    //Método para cálcular la nota definitiva de un estudiante
-    private static void calcularDefinitivaEstudiante(Estudiante estudiante1, Estudiante estudiante2, Estudiante estudiante3){
+    //Método para cálcular el promedio de notas del estudiante 1
+    private static double defEstudiante1(Estudiante estudiante1){
 
-        double definitiva1 = 0;
-        double definitiva2 = 0;
-        double definitiva3 = 0;
-
-        definitiva1 = (estudiante1.getNota1() + estudiante1.getNota2() + estudiante1.getNota3()) / 3;
-        definitiva2 = (estudiante2.getNota1() + estudiante2.getNota2() + estudiante2.getNota3()) / 3;
-        definitiva3 = (estudiante3.getNota1() + estudiante3.getNota2() + estudiante3.getNota3()) / 3;
-
-        System.out.println("La Nota definitiva del estudiante " + estudiante1.getNombre() + " es : " + definitiva1);
-        System.out.println("La Nota definitiva del estudiante " + estudiante2.getNombre() + " es : " + definitiva2);
-        System.out.println("La Nota definitiva del estudiante " + estudiante3.getNombre() + " es : " + definitiva3);
+            double defEstudiante1 = 0;
+            defEstudiante1 = (estudiante1.getNota1() + estudiante1.getNota2() + estudiante1.getNota3()) / 3;
+            return defEstudiante1;
     }
 
-    //Método para cálcular el promedio del curso
-    //private static void calcularPromedioCurso(Estudiante estudiante1, Estudiante estudiante2, Estudiante estudiante3){
+    //Método para cálcular el promedio de notas del estudiante 2
+    private static double defEstudiante2(Estudiante estudiante2){
 
-        //double promCurso = 0;
+        double defEstudiante2 = 0;
+        defEstudiante2 = (estudiante2.getNota1() + estudiante2.getNota2() + estudiante2.getNota3()) / 3;
+        return defEstudiante2;
+    }
 
-        //promCurso = ;
+    //Método para cálcular el promedio de notas del estudiante 3
+    private static double defEstudiante3(Estudiante estudiante3){
 
-        //System.out.println(promCurso);
+        double defEstudiante3 = 0;
+        defEstudiante3 = (estudiante3.getNota1() + estudiante3.getNota2() + estudiante3.getNota3()) / 3;
+        return defEstudiante3;
+    }
+
+    //Método para cálcular la nota definitiva de un estudiante
+    //private static void calcularDefinitivaEstudiante(Estudiante estudiante1, Estudiante estudiante2, Estudiante estudiante3){
+
+    //Scanner scanner = new Scanner(System.in);
+    //System.out.print("Digite el nombre del estudiante: ");
+
+    //String nomEstudiante = String.valueOf(scanner.nextInt());
+
+    //double definitiva = 0;
+
+    //definitiva =
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+        //double definitiva1 = 0;
+        //double definitiva2 = 0;
+        //double definitiva3 = 0;
+
+        //definitiva1 = (estudiante1.getNota1() + estudiante1.getNota2() + estudiante1.getNota3()) / 3;
+        //definitiva2 = (estudiante2.getNota1() + estudiante2.getNota2() + estudiante2.getNota3()) / 3;
+        //definitiva3 = (estudiante3.getNota1() + estudiante3.getNota2() + estudiante3.getNota3()) / 3;
+
+        //System.out.println("La Nota definitiva del estudiante " + estudiante1.getNombre() + " es : " + definitiva1);
+        //System.out.println("La Nota definitiva del estudiante " + estudiante2.getNombre() + " es : " + definitiva2);
+        //System.out.println("La Nota definitiva del estudiante " + estudiante3.getNombre() + " es : " + definitiva3);
     //}
+
+    //Método para cálcular el promedio del curso
+    private static void calcularPromedioCurso(Estudiante estudiante1, Estudiante estudiante2, Estudiante estudiante3){
+
+        double promCurso = 0;
+
+        promCurso = (defEstudiante1(estudiante1) + defEstudiante2(estudiante2) + defEstudiante3(estudiante3)) / 3;
+
+        System.out.println("El promedio del curso es : " + promCurso);
+    }
 
     //Método para cálcular el promedio de edad de los estudiantes
     private static void calcularPromedioEdad(Estudiante estudiante1, Estudiante estudiante2, Estudiante estudiante3) {
@@ -99,15 +143,76 @@ public class Main {
         System.out.println("El Promedio de la nota 1 de los estudiantes en el curso es: " + promNota1);
     }
 
-    //Método para cálcular la nota mayor del curso
-    private static void calcularNotaMayorDelCurso(Estudiante estudiante1, Estudiante estudiante2, Estudiante estudiante3) {
+    //Método para calcular la nota mayor del estudiante 1
+    private static double calcularNotaMayorEst1(Estudiante estudiante1) {
 
-        double notaMayor = 0;
+        double notaMayorEst1 = 0;
 
-        if (estudiante1.getNota1()>estudiante1.getNota2){
-
+        if (estudiante1.getNota1() > estudiante1.getNota2()) {
+            if (estudiante1.getNota1() > estudiante1.getNota3()) {
+                notaMayorEst1 = estudiante1.getNota1();
+                System.out.println(" La nota mayor del estudiante 1 es : " + notaMayorEst1);
+            } else {
+                notaMayorEst1 = estudiante1.getNota3();
+                System.out.println(" La nota mayor del estudiante 1 es : " + notaMayorEst1);
+            }
+        } else if (estudiante1.getNota2() > estudiante1.getNota3()) {
+            notaMayorEst1 = estudiante1.getNota2();
+            System.out.println(" La nota mayor del estudiante 1 es : " + notaMayorEst1);
+        } else {
+            notaMayorEst1 = estudiante1.getNota3();
+            System.out.println(" La nota mayor del estudiante 1 es : " + notaMayorEst1);
         }
-
-        System.out.println("El Promedio de la nota 1 de los estudiantes en el curso es: " + promNota1);
+        return notaMayorEst1;
     }
+
+    //Método para calcular la nota mayor del estudiante 2
+    private static double calcularNotaMayorEst2(Estudiante estudiante2) {
+
+        double notaMayorEst2 = 0;
+
+        if (estudiante2.getNota1() > estudiante2.getNota2()) {
+            if (estudiante2.getNota1() > estudiante2.getNota3()) {
+                notaMayorEst2 = estudiante2.getNota1();
+                System.out.println(" La nota mayor del estudiante 1 es : " + notaMayorEst2);
+            } else {
+                notaMayorEst2 = estudiante2.getNota3();
+                System.out.println(" La nota mayor del estudiante 1 es : " + notaMayorEst2);
+            }
+        } else if (estudiante2.getNota2() > estudiante2.getNota3()) {
+            notaMayorEst2 = estudiante2.getNota2();
+            System.out.println(" La nota mayor del estudiante 1 es : " + notaMayorEst2);
+        } else {
+            notaMayorEst2 = estudiante2.getNota3();
+            System.out.println(" La nota mayor del estudiante 1 es : " + notaMayorEst2);
+        }
+        return notaMayorEst2;
+    }
+
+    //Método para calcular la nota mayor del estudiante 3
+    private static double calcularNotaMayorEst3(Estudiante estudiante3) {
+
+        double notaMayorEst3 = 0;
+
+        if (estudiante3.getNota1() > estudiante3.getNota2()) {
+            if (estudiante3.getNota1() > estudiante3.getNota3()) {
+                notaMayorEst3 = estudiante3.getNota1();
+                System.out.println(" La nota mayor del estudiante 1 es : " + notaMayorEst3);
+            } else {
+                notaMayorEst3 = estudiante3.getNota3();
+                System.out.println(" La nota mayor del estudiante 1 es : " + notaMayorEst3);
+            }
+        } else if (estudiante3.getNota2() > estudiante3.getNota3()) {
+            notaMayorEst3 = estudiante3.getNota2();
+            System.out.println(" La nota mayor del estudiante 1 es : " + notaMayorEst3);
+        } else {
+            notaMayorEst3= estudiante3.getNota3();
+            System.out.println(" La nota mayor del estudiante 1 es : " + notaMayorEst3);
+        }
+        return notaMayorEst3;
+    }
+
+
+
+
 }
